3b0092d4b972259d98b5e6bc19f33665e782ab76
Jon Moore <jonr.moore75@gmail.com>
2016-06-18 19:56:08 +0100

    Refactor code in Main.cpp to new GameApp class
    Move main.cpp code to GameApp class.  In future we will improve this
    class to be more flexible and be split into core code and app specific
    code.

---
+++ Source/GameApp.cpp
@@ -0,0 +1,117 @@
+#include "GameApp.h"
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "SDLErrorReport.h"
+
+
+GameApp::GameApp()
+{
+
+}
+
+GameApp::~GameApp()
+{
+	Cleanup();
+}
+
+void GameApp::Cleanup()
+{
+	m_Texture.Release();
+	m_Window.Release();
+
+	// Shutdown SDL
+	IMG_Quit();
+	SDL_Quit();
+}
+
+bool GameApp::Init()
+{
+	// Initialise SDL, report error if it fails
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		Error2MsgBox("SDL Initialisation Failed!\n");
+		return false;
+	}
+
+	//Initialize PNG/JPG loading 
+	int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+	if (!(IMG_Init(imgFlags) & imgFlags))
+	{
+		std::string err_msg = "SDL_image could not initialize!\n";
+		err_msg += IMG_GetError();
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", err_msg.c_str(), nullptr);
+		return false;
+	}
+
+	// Create a window, report error if window not created
+	if (!m_Window.Create("Test Window",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640, 480,
+		SDL_WINDOW_SHOWN))
+	{
+		Error2MsgBox("Window Creation Failed.\n");
+		return false;
+	}
+
+	// Creates a renderer and clears the window
+	if (!m_Window.CreateRenderer())
+	{
+		Error2MsgBox("Renderer Creation Failed.\n");
+		return false;
+	}
+
+	// Load the image
+	if (!m_Texture.CreateFromFile(m_Window.GetRenderer(), "../Gfx/HelloWorld.png"))
+		return false;
+
+	return true;
+}
+
+void GameApp::HandleEvents()
+{
+	SDL_Event Event;
+
+	while (SDL_PollEvent(&Event))
+	{
+		if (Event.type == SDL_QUIT)
+			m_Running = false;
+	}
+}
+
+void GameApp::Render(Renderer& renderer)
+{
+	m_Texture.Render(renderer);
+	m_Window.Present();
+}
+
+void GameApp::MainLoop()
+{
+	m_Running = true;
+
+	// Application will indicate quit by setting m_Running to false
+	// until then we loop thro event handling and rendering each frame
+	while (m_Running)
+	{
+		HandleEvents();
+
+		// If we have valid window & renderer then render the frame
+		if (m_Window.CanRender())
+			Render(m_Window.GetRenderer());
+
+	}
+}
+
+int GameApp::Execute()
+{
+	// Initialise SDL and create window
+	if (!Init())
+		return -1;
+
+	MainLoop();
+
+	Cleanup();
+
+	return 0;
+}
---
+++ Source/GameApp.h
@@ -0,0 +1,34 @@
+#ifndef GameApp_h__
+#define GameApp_h__
+#pragma once
+
+#include "Window.h"
+#include "Texture.h"
+
+class GameApp
+{
+public:
+	GameApp();
+	~GameApp();
+
+	int Execute();
+
+private:
+
+	void Cleanup();
+
+	bool Init();
+
+	void HandleEvents();
+
+	void Render(Renderer& renderer);
+
+	void MainLoop();
+
+	bool	m_Running = false;
+
+	Window	m_Window;
+	Texture m_Texture;
+};
+
+#endif // GameApp_h__
