b9eef61cc20b2184311f39aa45f5586185adbb02
Jon Moore <jonr.moore75@gmail.com>
2017-08-25 23:21:07 +0100

    Extensive Refactor to Tidy Code
    - Separated the base collision code to free functions
    - Extract method on AppInit to make cleaner
    - Extract method on the BallPaddle Cllision code to make it cleaner

---
+++ Source/Collision.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum AXIS { XAXIS, YAXIS };
+class GameObject;
+class Vec2D;
+
+extern bool CheckForCircleAxisCollision(AXIS axis, int Norm, double planePos, GameObject& circle_obj, double circle_radius);
+bool CheckForCircleAxisIntersection(AXIS axis, int Norm, double planePos, GameObject& circle_obj, double circle_radius);
+
+double GetIntersectionDist(AXIS axis, int Norm, double planePos, GameObject& circle_obj, double circle_radius);
+
+double CheckCornerCircleCollision(Vec2D& corner_pos, Vec2D& circle_pos, Vec2D& relVelocity, double circle_radius);
+
